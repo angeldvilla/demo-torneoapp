6d73de8ad642e2ffec89955a6223c82887991033
Angel David Villa Luján <escarlata.2003@gmail.com>
2026-04-12 12:27:01 -0500

    update promise string in route for groups

--- src/app/api/grupos/[torneoId]/route.ts
+++ src/app/api/grupos/[torneoId]/route.ts
@@ -2,8 +2,9 @@ import { db } from "@/lib/db";
 
 export async function GET(
   req: Request,
-  { params }: { params: { torneoId: string } }
+  context: { params: Promise<{ torneoId: string }> }
 ) {
+  const params = await context.params;
   const [rows] = await db.query(
     "SELECT * FROM grupos WHERE torneoId = ?",
     [params.torneoId]
